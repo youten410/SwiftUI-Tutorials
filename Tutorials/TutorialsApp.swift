f5fb280d0e9ae2fb2d265bc005828d649119a813
Shun Sato <techregisteremail@gmail.com>
2023-01-16 23:21:23 +0900

    Initial Commit

---
+++ Tutorials/TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialsApp.swift
+//  Tutorials
+//
+//  Created by Shun Sato on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
