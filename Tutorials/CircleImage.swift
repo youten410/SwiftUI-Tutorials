125637b20b8adb9be226e79913d55b1315b28f5c
Shun Sato <techregisteremail@gmail.com>
2023-01-16 23:51:48 +0900

    add: CircleImage,MapView

---
+++ Tutorials/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Tutorials
+//
+//  Created by Shun Sato on 2023/01/16.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
